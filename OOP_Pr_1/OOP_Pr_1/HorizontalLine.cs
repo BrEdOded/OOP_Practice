6914a7bf98ed69c3e7358fd81306c6b2fdc11b17
BrEdOded <84377813+BrEdOded@users.noreply.github.com>
2021-11-02 14:19:01 +1000

    Add files via upload

---
+++ OOP_Pr_1/OOP_Pr_1/HorizontalLine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Pr_1
+{
+    class HorizontalLine : Figure
+    {
+        public HorizontalLine(int xLeft, int xRight, int y, char Symbol)
+        {
+            PointList = new List<Point>();
+            for (int x = xLeft; x <= xRight; x++)
+            {
+                Point Point = new Point(x, y, Symbol);
+                PointList.Add(Point);
+            }  
+        }
+    }
+}
---
+++ OOP_Pr_1/OOP_Pr_1/Snake.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Pr_1
+{
+    class Snake : Figure
+    {
+        public Snake(Point Tail, int Lenght, Direction Direction) 
+        {
+            //List<Point> PointList = new List<Point>();
+            for (int i = 0; i < Lenght; i++)
+            {
+                Point Point = new Point(Tail);
+                Point.Move(i, Direction);
+                PointList.Add(Point);
+            }
+        }
+    }
+}
