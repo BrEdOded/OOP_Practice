e1a80325f9943bf7e247df07b945d06b0bcc715b
BrEdOded <84377813+BrEdOded@users.noreply.github.com>
2021-11-02 11:44:32 +1000

    Add files via upload

--- OOP_Pr_1/OOP_Pr_1/Program.cs
+++ OOP_Pr_1/OOP_Pr_1/Program.cs
@@ -10,8 +10,17 @@ namespace OOP_Pr_1
     {
         static void Main()
         {
-             Console.WriteLine("Hello World!");
-             Console.ReadLine();
+            Point Point1 = new Point();
+            Point1.x = 1;
+            Point1.y = 3;
+            Point1.Symbol = '*';
+            Point1.DrawPoint();
+
+            Point Point2 = new Point();
+            Point2.x = 4;
+            Point2.y = 5;
+            Point2.Symbol = '#';
+            Point2.DrawPoint();
         }
     }
 }
