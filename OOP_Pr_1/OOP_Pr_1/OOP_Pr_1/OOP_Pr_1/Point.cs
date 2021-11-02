8e6cc0b5398b8425c0f9e3aeffbe96f29882638c
BrEdOded <84377813+BrEdOded@users.noreply.github.com>
2021-11-02 11:55:42 +1000

    Add files via upload

---
+++ OOP_Pr_1/OOP_Pr_1/OOP_Pr_1/OOP_Pr_1/Point.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Pr_1
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char Symbol;
+        public Point()
+        {
+            //Console.WriteLine("Создается новая точка");
+        }
+        public Point(int _x, int _y, char _Symbol)
+        {
+            x = _x;
+            y = _y;
+            Symbol = _Symbol;
+        }
+
+        public void DrawPoint()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(Symbol);
+        }
+    }
+}
