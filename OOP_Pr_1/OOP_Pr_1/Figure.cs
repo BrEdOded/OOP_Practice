723d98663cfda6282513d2c689cf485a9349e0fc
BrEdOded <84377813+BrEdOded@users.noreply.github.com>
2021-11-02 17:16:42 +1000

    Add files via upload

---
+++ OOP_Pr_1/OOP_Pr_1/Figure.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Pr_1
+{
+    class Figure
+    {
+        protected List<Point> PointList = new List<Point>();
+        public virtual void DrawLine()
+        {
+            foreach (Point Point in PointList)
+            {
+                Point.DrawPoint();
+            }
+        }
+    }
+}
