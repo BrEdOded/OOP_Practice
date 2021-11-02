92317190b7f73db04116b103005ebc23cf8310b3
BrEdOded <84377813+BrEdOded@users.noreply.github.com>
2021-11-02 18:31:16 +1000

    Add files via upload

---
+++ OOP_Pr_1/OOP_Pr_1/Figure.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Pr_1
+{
+    class Figure
+    {
+        protected List<Point> PointList = new List<Point>();
+        public virtual void DrawLine()
+        {
+            foreach (Point Point in PointList)
+            {
+                Point.DrawPoint();
+            }
+        }
+
+        internal bool IsHit(Figure figure)
+        {
+            foreach (var Point in PointList)
+            {
+                if (figure.IsHit(Point)) return true;
+            }
+            return false;
+        }
+        private bool IsHit(Point point)
+        {
+            foreach (var Point in PointList)
+            {
+                if (Point.IsHit(point)) return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ OOP_Pr_1/OOP_Pr_1/Walls.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Pr_1
+{
+    class Walls
+    {
+        List<Figure> WallList;
+
+        public Walls(int MapWitdh, int MapHeight)
+        {
+            WallList = new List<Figure>();
+
+            //Отрисовка рамки
+            HorizontalLine UpHorizontalLine = new HorizontalLine(0, MapWitdh-2, 0, '-');
+            HorizontalLine DownHorizontalLine = new HorizontalLine(0, MapWitdh - 2, MapHeight-1, '-');
+            VerticalLine LeftVerticalLine = new VerticalLine(0, MapHeight - 1, 0, '|');
+            VerticalLine RightVerticalLine = new VerticalLine(0, MapHeight - 1, MapWitdh - 2, '|');
+
+            WallList.Add(UpHorizontalLine);
+            WallList.Add(DownHorizontalLine);
+            WallList.Add(LeftVerticalLine);
+            WallList.Add(RightVerticalLine);
+        }
+
+        internal bool IsHit(Figure figure)
+        {
+            foreach (var Wall in WallList)
+            {
+                if (Wall.IsHit(figure)) return true;
+            }
+            return false;
+        }
+
+        public void DrawWall()
+        {
+            foreach (var Wall in WallList) Wall.DrawLine();
+        }
+    }
+}
