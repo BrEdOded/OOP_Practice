12b24e0ec965abf15435192b40d159c44a6a445f
BrEdOded <84377813+BrEdOded@users.noreply.github.com>
2021-11-02 16:37:56 +1000

    Add files via upload

--- OOP_Pr_1/OOP_Pr_1/Point.cs
+++ OOP_Pr_1/OOP_Pr_1/Point.cs
@@ -9,21 +9,54 @@ namespace OOP_Pr_1
         public int x;
         public int y;
         public char Symbol;
-        public Point()
-        {
-            //Console.WriteLine("Создается новая точка");
-        }
         public Point(int _x, int _y, char _Symbol)
         {
             x = _x;
             y = _y;
             Symbol = _Symbol;
+        } 
+        public Point(Point Point)
+        {
+            x = Point.x;
+            y = Point.y;
+            Symbol = Point.Symbol;
         }
-
         public void DrawPoint()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(Symbol);
+        }
+        public void Move(int OffSet, Direction Direction)
+        {
+            if(Direction == Direction.RIGHT)
+            {
+                x = x + OffSet;
+            }
+            else if(Direction == Direction.LEFT)
+            {
+                x = x - OffSet;
+            }
+            else if(Direction == Direction.UP)
+            {
+                y = y - OffSet;
+            }
+            else if (Direction == Direction.DOWN)
+            {
+                y = y + OffSet;
+            }
+        }
+        public override string ToString()
+        {
+            return x + ", " + y + ", " + Symbol;
+        }
+        public void Clear()
+        {
+            Symbol = ' ';
+            DrawPoint();
+        }
+        public bool IsHit(Point Point)
+        {
+            return Point.x == this.x && Point.y == this.y;
         }
     }
 }
--- OOP_Pr_1/OOP_Pr_1/Program.cs
+++ OOP_Pr_1/OOP_Pr_1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace OOP_Pr_1
@@ -10,65 +11,50 @@ namespace OOP_Pr_1
     {
         static void Main()
         {
-            // Первое п/з
-            /*
-             Console.WriteLine("Hello World!");
-             Console.ReadLine();
-            */
+            Console.SetBufferSize(200, 50);
 
-            // Второе п/з
-            /*
-            static void DrawPoint(int x, int y, char Symbol)
+            //Отрисовка рамки
+            HorizontalLine UpHorizontalLine = new HorizontalLine(0,78,0,'-');
+            UpHorizontalLine.DrawLine();
+            HorizontalLine DownHorizontalLine = new HorizontalLine(0, 78, 24, '-');
+            DownHorizontalLine.DrawLine();
+            VerticalLine LeftVerticalLine = new VerticalLine(0, 24, 0, '|');
+            LeftVerticalLine.DrawLine();
+            VerticalLine RightVerticalLine = new VerticalLine(0, 24, 78, '|');
+            RightVerticalLine.DrawLine();
+
+            //Отрисовка точек
+            Point Point1 = new Point(4,5,'*');
+            Point1.DrawPoint();
+
+            //Отрисовка змейки
+            Snake Snake = new Snake(Point1, 4, Direction.RIGHT);
+            Snake.DrawLine();
+
+            //Создание еды
+            FoodCreator FoodCreator = new FoodCreator(80, 25, '$');
+            Point Food = FoodCreator.CreateFood();
+            Food.DrawPoint();
+
+            //Перемещение змейки
+            while(true)
             {
-                Console.SetCursorPosition(x, y);
-                Console.Write(Symbol);
+                if (Snake.Eat(Food))
+                {
+                    Food = FoodCreator.CreateFood();
+                    Food.DrawPoint();
+                }
+                else Snake.Move();
+                Thread.Sleep(100);
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo Key = Console.ReadKey();
+                    Snake.ChangeTheWay(Key.Key);
+                }
+                Thread.Sleep(200);
+                Snake.Move();
             }
-            int x = 1;
-            int y = 3;
-            char Symbol = '*';
-
-            // Console.SetCursorPosition(x, y);
-            // Console.Write(Symbol);
-            DrawPoint(x, y, Symbol);
-
-            int x1 = 4;
-            int y1 = 5;
-            char Symbol1 = '#';
-
-            //Console.SetCursorPosition(x1, y1);
-            //Console.Write(Symbol1);
-            DrawPoint(x1, y1, Symbol1);
-            */
-
-            // Третье п/з
-            /*
-            Point Point1 = new Point();
-            Point1.x = 1;
-            Point1.y = 3;
-            Point1.Symbol = '*';
-            Point1.DrawPoint();
-
-            Point Point2 = new Point();
-            Point2.x = 4;
-            Point2.y = 5;
-            Point2.Symbol = '#';
-            Point2.DrawPoint();
-            */
-
-            // Четвертое п/з
-            /**/
-            Point Point1 = new Point();
-            /*Point1.x = 1;
-            Point1.y = 3;
-            Point1.Symbol = '*';*/
-            Point1.DrawPoint();
-
-            Point Point2 = new Point();
-           /* Point2.x = 4;
-            Point2.y = 5;
-            Point2.Symbol = '#';*/
-            Point2.DrawPoint();
-
         }
     }
 }
